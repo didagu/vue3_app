c080567540beab0a941228f6708a99a2dae2e161
didagu.cloud <dominic.abas@abas.de>
2020-08-17 09:54:25 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <app-layout
+      :no-toolbar="!showToolbar"
+      :show-banner="showBanner"
+      no-main-padding
+    >
+      <template #banner>
+        <div
+          class="banner"
+          style="flex-grow: 1;padding: 8px; align-self: center; display: flex;align-content:center;"
+        >
+          <marquee>
+            This is the end!!!! Finally somebody is using
+            Marqueeeeeeeeeeeeeeeeeeeeeee! Super promo: buy 2 abas web devs,
+            get one for free!
+          </marquee>
+        </div>
+      </template>
+      <h1>My Event</h1>
+      <p>Capacity: {{ capacity }}</p>
+      <button @click="increaseCapacity()">Increase Capacity</button>
+    </app-layout>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import AppLayout from '@abas/elements/src/components/AppLayout.vue';
+import { ref } from 'vue'; // <-- Use this line if you're in a Vue 3 app
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    AppLayout,
+  },
+  setup() {
+    const capacity = ref(3);
+    const showToolBar = ref(true);
+    const showBanner = ref(true);
+
+    function increaseCapacity() {
+      capacity.value += 1;
+    }
+
+    return {
+      capacity,
+      showToolBar,
+      showBanner,
+      increaseCapacity,
+    };
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
